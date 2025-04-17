db80123c79de91acddb634c1a0de3b38eb7ad05c
ChromaXard <axel.kastler6847@gmail.com>
2025-04-17 16:47:39 +0200

    adding some languages on skills and rework the visuak of language component

--- src/app/[locale]/skills/page.tsx
+++ src/app/[locale]/skills/page.tsx
@@ -1,7 +1,7 @@
 import { Metadata } from "next";
 import React from "react";
 import { useTranslations } from "next-intl";
-import LanguageProgress from "@/app/ui/languageProgress";
+import LanguagePresentation from "@/app/ui/languagePresentation";
 
 export const metadata: Metadata = {
 	title: "A propos - Portfolio - akastler",
@@ -12,25 +12,37 @@ export default function About() {
 	const t = useTranslations("skills");
 	return (
 		<div className="m-4 flex-1">
-			<div className="p-4 flex-col lg:flex-row bg-gray-400 rounded-[2vw] w-full">
-				<h1 className="text-3xl">{t("title")}</h1>
+			<div className="p-4 flex-col lg:flex-row bg-gray-300 rounded-[2vw] w-full">
+				<h1 className="text-5xl">{t("title")}</h1>
 				<p className="text-lg mt-2">{t("description")}</p>
 				<div className="grid grid-cols-1 lg:grid-cols-2 gap-4 mt-4">
-          <div className="bg-gray-500 lg:col-span-2 h-25 rounded-[1vw] flex items-center justify-center">
-            <h2 className="text-2xl">
-              {t("webDev")}
-            </h2>
-          </div>
+					<div className="lg:col-span-2 h-25 flex items-center justify-center border-b-2">
+						<h2 className="text-4xl">{t("webDev")}</h2>
+					</div>
 					<div>
-            <LanguageProgress name="html5" percent={70} />
-          </div>
+						<LanguagePresentation name="html5" />
+					</div>
 					<div>
-            <LanguageProgress name="css3" percent={10} />
-          </div>
-					<div className="bg-gray-500 h-25 rounded-[1vw]"></div>
-					<div className="bg-gray-500 h-25 rounded-[1vw]"></div>
-					<div className="bg-gray-500 h-25 rounded-[1vw]"></div>
-					<div className="bg-gray-500 h-25 rounded-[1vw]"></div>
+						<LanguagePresentation name="css3" />
+					</div>
+					<div>
+						<LanguagePresentation name="javascript" />
+					</div>
+					<div>
+						<LanguagePresentation name="typescript" />
+					</div>
+					<div>
+						<LanguagePresentation name="bootstrap" />
+					</div>
+					<div>
+						<LanguagePresentation name="tailwind" />
+					</div>
+					<div>
+						<LanguagePresentation name="react" />
+					</div>
+					<div>
+						<LanguagePresentation name="nextjs" />
+					</div>
 					<div className="bg-gray-500 h-25 rounded-[1vw] lg:col-span-2 lg:justify-self-center lg:w-1/2"></div>
 				</div>
 			</div>
---
+++ src/app/ui/languagePresentation.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+import { useTranslations } from "next-intl";
+export default function LanguagePresentation({ name }: { name: string }) {
+	const t = useTranslations("skills");
+	const projects = t.raw(`detail.${name}.projects`);
+	return (
+		<div className="p-2 h-full w-full flex flex-row">
+			<div className="w-2/8 flex items-center justify-center">
+					<Image
+						src={`/${name}.svg`}
+						width={150}
+						height={150}
+						alt={`${name} svg logo`}
+					/>
+			</div>
+			<div className="flex flex-col ml-3 w-6/8">
+				<div>
+					<h3 className="text-2xl font-bold">{name.toUpperCase()}</h3>
+				</div>
+				<div>
+					<p className="text-md pl-2">{t(`detail.${name}.description`)}</p>
+				</div>
+				<div className="mt-2">
+					<h4 className="text-lg">{t("detail.usedWith")}</h4>
+					<ul className="list-disc list-inside pl-2">
+						{projects.map((project: string, index: number) => (
+							<li key={index} className="text-md">
+								<span>{project}</span>
+							</li>
+						))}
+					</ul>
+				</div>
+			</div>
+		</div>
+	);
+}
